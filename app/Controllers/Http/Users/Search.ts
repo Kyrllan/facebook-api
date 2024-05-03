9d2b214d2594ada6ee36afd13c35ca0b8c7fb404
Kyrllan <kyrllan@hotmail.com>
2024-05-03 17:42:35 -0300

    feito desafio de buscar usuário

--- app/Controllers/Http/Users/Search.ts
+++ app/Controllers/Http/Users/Search.ts
@@ -1,5 +1,19 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { User } from "App/Models";
 
 export default class SearchController {
-  public async show({ params }: HttpContextContract) {}
+  public async index({ request, response }: HttpContextContract) {
+    try {
+      const { keyword } = request.qs();
+
+      const users = await User.query()
+        .where("name", "LIKE", `%${keyword}%`)
+        .orWhere("username", "LIKE", `%${keyword}%`)
+        .orWhere("email", "LIKE", `%${keyword}%`);
+
+      return response.ok({ users });
+    } catch (error) {
+      return response.status(500).json({ message: "Erro interno do servidor" });
+    }
+  }
 }
