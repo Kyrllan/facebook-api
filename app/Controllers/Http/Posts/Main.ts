8d5246754fe3dd39e96fdb702ef6c09374cca772
Kyrllan <kyrllan@hotmail.com>
2024-05-06 15:44:15 -0300

    created model, validator and migration of posts

--- app/Controllers/Http/Posts/Main.ts
+++ app/Controllers/Http/Posts/Main.ts
@@ -1,13 +1,65 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { StoreValidator, UpdateValidator } from "App/Validators/Post/Main";
 import { User, Post } from "App/Models";
+import Application from "@ioc:Adonis/Core/Application";
+import fs from "fs";
 
 export default class PostsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request, auth }: HttpContextContract) {
+    const { username } = request.qs();
 
-  public async store({}: HttpContextContract) {}
+    const user = (await User.findBy("username", username)) || auth.user!;
 
-  public async update({}: HttpContextContract) {}
+    await user.load("posts", (query) => {
+      query.preload("user", (query) => {
+        query.select(["id", "name", "username"]);
+        query.preload("avatar");
+      });
+    });
 
-  public async destroy({}: HttpContextContract) {}
+    return user.posts;
+  }
+
+  public async store({ request, auth }: HttpContextContract) {
+    const data = await request.validate(StoreValidator);
+    const post = await auth.user!.related("posts").create(data);
+
+    return post;
+  }
+
+  public async update({
+    request,
+    response,
+    params,
+    auth,
+  }: HttpContextContract) {
+    const data = await request.validate(UpdateValidator);
+    const post = await Post.findOrFail(params.id);
+
+    if (auth.user?.id !== post.userId) {
+      return response.unauthorized();
+    }
+
+    await post.merge(data).save();
+
+    return post;
+  }
+
+  public async destroy({ response, params, auth }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id);
+
+    if (auth.user?.id !== post.userId) {
+      return response.unauthorized();
+    }
+
+    await post.load("media");
+
+    if (post.media) {
+      fs.unlinkSync(Application.tmpPath("uploads", post.media.fileName));
+
+      await post.media.delete();
+    }
+
+    await post.delete();
+  }
 }
