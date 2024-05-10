e86c1252eb21846293325bff032b2daa68fa70ea
Kyrllan <kyrllan@hotmail.com>
2024-05-10 15:54:11 -0300

    created reactions resource

--- app/Controllers/Http/Reactions/Main.ts
+++ app/Controllers/Http/Reactions/Main.ts
@@ -1,7 +1,23 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { UpdateValidator } from "App/Validators/Reactions/Main";
+import { Post, Reaction } from "App/Models";
 
 export default class ReactionsController {
-  public async update({}: HttpContextContract) {}
+  public async update({ request, auth }: HttpContextContract) {
+    const { type, postId } = await request.validate(UpdateValidator);
 
-  public async destroy({}: HttpContextContract) {}
+    const post = await Post.findOrFail(postId);
+
+    const reaction = await post
+      .related("reactions")
+      .updateOrCreate({ postId, userId: auth.user!.id }, { type });
+
+    return reaction;
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const reaction = await Reaction.findOrFail(params.id);
+
+    await reaction.delete();
+  }
 }
